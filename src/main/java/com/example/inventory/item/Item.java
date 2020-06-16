db42148beca147977e4d8042b94afdb07e29d7e7
sayeemu <sayeemkw@gmail.com>
2020-06-16 17:46:50 +0530

    Report number incrementation function added

--- src/main/java/com/example/inventory/item/Item.java
+++ src/main/java/com/example/inventory/item/Item.java
@@ -1,16 +1,9 @@
 package com.example.inventory.item;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import com.example.inventory.itemInventory.Inventory;
 
 
 
--- src/main/java/com/example/inventory/report/ReportService.java
+++ src/main/java/com/example/inventory/report/ReportService.java
@@ -1,6 +1,10 @@
 package com.example.inventory.report;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
-import com.example.inventory.item.ItemRepository;
 import com.example.inventory.itemInventory.Inventory;
 import com.example.inventory.itemInventory.InventoryRepository;
 
@@ -31,6 +34,8 @@ public class ReportService {
 
     public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
         String path = "C:\\Users\\sayee\\Desktop\\Reports";
+        
+        
         List<Report> InventoryReport = new ArrayList<>();
         for(Inventory i:inventoryRepository.findAll()) {
         	Report report = new Report(i.getId(),i.getItem().getName() , i.getItem().getCost() ,i.getStock());
@@ -41,13 +46,42 @@ public class ReportService {
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(InventoryReport);
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("createdBy", "s");
+        int count = getCount(path);            
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
         if (reportFormat.equalsIgnoreCase("html")) {
-            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\InventoryReport.html");
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\InventoryReport" + count + ".html");
         }
         if (reportFormat.equalsIgnoreCase("pdf")) {
-            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\InventoryReport.pdf");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\InventoryReport" + count + ".pdf");
         }
+        count++;
+        putCount(count,path);
         return "report generated in path : " + path;
+    }
+    
+    public int getCount(String path) {
+        int count = 0;
+        try {
+            if ( !new File("classpath:\\myCount.txt").exists())
+                return 1;
+            else {
+                BufferedReader br = new BufferedReader(new FileReader(new File(path+"\\myCount.txt")));
+                count = Integer.parseInt(br.readLine());
+                br.close();
+            }                
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    public void putCount(int count, String path) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path + "\\myCount.txt")));
+            bw.write(Integer.toString(count));
+            bw.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
